b5495fa9c91b6c094d62b12e02eb8da627bb07f5
DHARAVATH USHARANI <ushadharavath786@gmail.com>
2023-02-22 11:32:08 +0530

    hi

---
+++ myfirstreactappnpx/src/plan.js
@@ -0,0 +1,7 @@
+function Plan(props){
+    return <>
+    <li className="shadow p-2 my-2 col-sm-9"></li>
+    <button className="btn btn-danger my-2 col-sm-2"> x </button>
+    </> 
+}
+export default Plan;
